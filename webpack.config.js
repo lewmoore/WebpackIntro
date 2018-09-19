831d1ae4052b57eec39f065f1c45a0d5c9065af4
Lewis Moore <lewis.moore2707@gmail.com>
2018-09-19 14:35:28 +0100

    Add Webpack config with Babel loader

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: [
+        '.app/index.js'
+    ],
+    module: {
+        loaders: [{
+            test: /\.js$/, 
+            include:  __dirname + '/app',
+            loader: 'babel?presets[]=es2015'
+        }]
+    }
+}
