5ee86d3547753a8f7eebce0996d18d2ec8d266c2
Lewis Moore <lewis.moore2707@gmail.com>
2018-09-19 15:57:19 +0100

    Add correct webpack config. Add index.js with es6 syntax and render to index. Omit dist directory

---
+++ app/index.js
@@ -0,0 +1,4 @@
+const appDiv = document.getElementById('app');
+const name = 'Lewis';
+
+appDiv.innerHTML = `Welcome to ${name}'s webpack tutorial!`;
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
 let HtmlWebpackPlugin = require('html-webpack-plugin')
 
-let HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+let HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
     template: __dirname + '/app/index.html',
     filename: 'index.html'
   });
 
 module.exports = {
     entry: [
-        '.app/index.js'
+        './app/index.js'
     ],
     module: {
-        loaders: [{
+        rules: [{
             test: /\.js$/, 
-            include:  __dirname + '/app',
-            loader: 'babel?presets[]=es2015'
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
         }]
     },
     output: {
         filename: 'index_bundle.js',
         path: __dirname + '/dist'
     },
-    plugins: [HtmlWebpackPluginConfig]
+    plugins: [HTMLWebpackPluginConfig]
 }
